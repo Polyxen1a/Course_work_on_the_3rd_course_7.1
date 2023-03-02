b9b33b160addb08fb1e83e1451a6cee7fb44a7aa
Polyxen1a <maslowa@inbox.ru>
2023-03-02 17:41:18 +0300

    Initial commit

---
+++ src/main/java/Course_work_on_the_3rd_course_7/CourseWorkOnThe3rdCourse7Application.java
@@ -0,0 +1,13 @@
+package Course_work_on_the_3rd_course_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOnThe3rdCourse7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkOnThe3rdCourse7Application.class, args);
+	}
+
+}
---
+++ src/test/java/Course_work_on_the_3rd_course_7/CourseWorkOnThe3rdCourse7ApplicationTests.java
@@ -0,0 +1,13 @@
+package Course_work_on_the_3rd_course_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOnThe3rdCourse7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
